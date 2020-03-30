61f7db36394c61d69f055dd7f96ba78f2c78f7cd
xavL <xou.xavl@gmail.com>
2020-03-30 10:51:15 +0200

    Formatage => black

--- langage-python/decouverte/afficher_saisir.py
+++ langage-python/decouverte/afficher_saisir.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # version 2.7.17
 
-print("Affichage sur la sortie standard")
+print ("Affichage sur la sortie standard")
 
-nom = raw_input('Nom :')
-prenom = raw_input('Prenom :')
+nom = raw_input("Nom :")
+print type(nom)
+
+prenom = raw_input("Prenom :")
+print type(prenom)
 
 print "Bonjour", prenom, nom
