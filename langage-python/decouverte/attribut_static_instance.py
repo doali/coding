2f075c7878eea64ad28299dfc2b0a265712a32c8
xavL <xou.xavl@gmail.com>
2020-03-30 11:06:59 +0200

    Formatage => black

--- langage-python/decouverte/attribut_static_instance.py
+++ langage-python/decouverte/attribut_static_instance.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python2
 
-class Example:
-    Variable = 2           # static variable
 
-print Example.Variable     # prints 2   (static variable)
+class Example:
+    Variable = 2  # static variable
+
+
+print Example.Variable  # prints 2   (static variable)
 
 # Access through an instance
 instance = Example()
-print instance.Variable    # still 2  (ordinary variable)
-
+print instance.Variable  # still 2  (ordinary variable)
 
 # Change within an instance
-instance.Variable = 3      #(ordinary variable)
-print instance.Variable    # 3   (ordinary variable)
-print Example.Variable     # 2   (static variable)
-
+instance.Variable = 3  # (ordinary variable)
+print instance.Variable  # 3   (ordinary variable)
+print Example.Variable  # 2   (static variable)
 
 # Change through Class
-Example.Variable = 5       #(static variable)
-print instance.Variable    # 3  (ordinary variable)
-print Example.Variable     # 5  (static variable)
+Example.Variable = 5  # (static variable)
+print instance.Variable  # 3  (ordinary variable)
+print Example.Variable  # 5  (static variable)
+
+# Other behaviour
+instance2 = Example()
+print instance2.Variable  # 5 pointe sur Example.Variable
+Example.Variable = 10
+print instance2.Variable  # 10 pointe toujours sur Example.Variable
+instance2.Variable = 11  # pointe desormais sur 11
+print instance2.Variable  # 11
+print Example.Variable  # 10
