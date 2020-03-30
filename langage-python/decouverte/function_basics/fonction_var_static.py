944793ac933f89990bd5d3038d7b590270dccc12
xavL <xou.xavl@gmail.com>
2020-03-30 12:30:51 +0200

    Reorganisation des répertoires et fichiers

--- langage-python/decouverte/function_basics/fonction_var_static.py
+++ langage-python/decouverte/function_basics/fonction_var_static.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python2
 
+
 def some_fun():
-    some_fun.i += 1
+    some_fun.i += 1  # variable locale static
     print(some_fun.i)
 
 
 def main():
-    some_fun.i = 0;
+    some_fun.i = 0
+    # declaration initialisation
     some_fun()
     some_fun()
     some_fun()
