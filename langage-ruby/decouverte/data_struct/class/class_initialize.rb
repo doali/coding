6e638a49f5b7f1819cae72843012efe041ec65f5
xavL <xou.xavl@gmail.com>
2020-04-04 08:49:02 +0200

    Utilisation de la méthode : initialize - def initialize[(arg1, arg2=<val_defaut>, ...)] [...] end - <MaClass>.new[(arg1, arg2=<val_defaut>, ...)] <= invoquera en premier apple la méthode initialize
    Redefinition de la methode : to_s
    - def to_s [...] end
    - puts <mon_instance> <= invoquera la methode to_s

---
+++ langage-ruby/decouverte/data_struct/class/class_initialize.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+class MiniFB
+  # === attributs
+  attr_accessor :prenom
+  attr_accessor :amis
+
+  # === initialisation
+  def initialize(prenom, amis=[])
+    self.prenom = prenom
+    @amis = amis
+  end
+
+  # === methodes
+  def nb_amis
+    return amis.size
+  end
+
+  def to_s
+    des = (nb_amis  > 1) ? "s" : ""
+    return "[" + nb_amis.to_s + "] ami" + des + " compte pour " + prenom
+  end
+end
+
+# Instanciation de MiniFB
+linus = MiniFB.new("Linus")
+larry = MiniFB.new("Larry", [linus])
+brian = MiniFB.new("Brian", [linus, larry])
+
+puts linus
+puts larry
+puts brian
