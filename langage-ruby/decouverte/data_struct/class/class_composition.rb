719a0d3058d6f8612abea6cb9fa33cb8ebcf3aab
xavL <xou.xavl@gmail.com>
2020-04-05 13:43:20 +0200

    Composition - exemple de code
    Documentation
    - composition

--- langage-ruby/decouverte/data_struct/class/class_composition.rb
+++ langage-ruby/decouverte/data_struct/class/class_composition.rb
@@ -1,96 +1,52 @@
 #!/usr/bin/env ruby
 
 class Panier
-  attr_accessor :_nom
-  attr_accessor :_max_legumes
   attr_accessor :_legumes # composition
 
-  def initialize(nom, max_legumes)
-    @_nom = nom
-    @_max_legumes = max_legumes
+  def initialize
     @_legumes = []
   end
 
-  def est_plein
-    return _legumes.size == _max_legumes
-  end
-
   def ajouter(legume)
-    _legumes << legume if ! est_plein
+    _legumes << legume
   end
 
-  def remplir(legumes)
-    legumes.each do |legume|
-      ajouter(legume)
-    end
-  end
-
-  def description
-    puts "Panier de " + _nom
-    puts "Contenu :"
+  def to_s
+    description = "Panier :" + ((_legumes.size == 0) ? "vide" : "")
     _legumes.each do |legume|
-      puts "- " + legume.to_s
+      description += "\n"
+      description += ("- " + legume.to_s)
     end
-    puts "Total :[" + _legumes.size.to_s + "/" + _max_legumes.to_s + "]"
+    return description
   end
 end
 
 
 class Legume
   attr_accessor :_nom
+  attr_accessor :_famille
 
-  def initialize(nom)
+  def initialize(nom, famille)
     @_nom = nom
+    @_famille = famille
   end
 
   def to_s
-    return _nom
+    return "Legume :{" + _nom + ":" + _famille + "}"
   end
 end
 
-
-class Etale
-  # attribut static
-  @@LISTE_LEGUME = [
-    "tomate",
-    "poivron",
-    "patate"
-  ]
-
-  # methode static
-  def self.cree_etale(quantite)
-    etale = []
-    quantite.times do
-      etale << Legume.new(legume_au_hasard)
-    end
-    return etale
-  end
-
-  # methode static
-  def self.legume_au_hasard
-    return @@LISTE_LEGUME[
-      rand(@@LISTE_LEGUME.size) # entre 0 et (LISTE_LEGUME.size - 1)
-    ]
-  end
-
-  # methode static
-  def self.desription(etale)
-    puts "Etale:"
-    etale.each do |legume|
-      puts "- " + legume.to_s
-    end
-  end
-end
 
 # --------------------------------------------------
-# Instanciations....
-etale = Etale.cree_etale(10) # utilisation de methodes static
-Etale.desription(etale)      # utilisation de methodes static
+legume_tomate = Legume.new("tomate", "fruit")
+legume_patate = Legume.new("patate", "racine")
+puts legume_tomate
+puts legume_patate
 
-puts "---"
-panier = Panier.new("Georges", 7) # methodes d'instance + composition
-panier.description
+puts
+panier = Panier.new # methodes d'instance + composition
+puts panier
 
-puts "---"
-panier.remplir(etale)
-panier.description
+panier.ajouter(legume_tomate)
+panier.ajouter(legume_patate)
+puts panier
--- langage-ruby/doc/decouverte.md
+++ langage-ruby/doc/decouverte.md
@@ -283,7 +283,69 @@ le fils
 B:ClassFils
 ```
 ### Composition
+- `attr_attr_accessor :<nom_composant>` : `<nom_composant>` est une reference vers l'objet pour lequel on définit une relation de composition
+```ruby
+class Panier
+  attr_accessor :_legumes # composition
 
+  def initialize
+    @_legumes = []
+  end 
+
+  def ajouter(legume)
+    _legumes << legume
+  end 
+
+  def to_s
+    description = "Panier :" + ((_legumes.size == 0) ? "vide" : "") 
+    _legumes.each do |legume|
+      description += "\n"
+      description += ("- " + legume.to_s)
+    end 
+    return description
+  end 
+end
+
+
+class Legume
+  attr_accessor :_nom
+  attr_accessor :_famille
+
+  def initialize(nom, famille)
+    @_nom = nom 
+    @_famille = famille
+  end 
+
+  def to_s
+    return "Legume :{" + _nom + ":" + _famille + "}" 
+  end 
+end
+
+
+# --------------------------------------------------
+legume_tomate = Legume.new("tomate", "fruit")
+legume_patate = Legume.new("patate", "racine")
+puts legume_tomate
+puts legume_patate
+
+puts
+panier = Panier.new # methodes d'instance + composition
+puts panier
+
+panier.ajouter(legume_tomate)
+panier.ajouter(legume_patate)
+puts panier
+```
+qui produit le résultat suivant
+```bash
+Legume :{tomate:fruit}
+Legume :{patate:racine}
+
+Panier :vide
+Panier :
+- Legume :{tomate:fruit}
+- Legume :{patate:racine}
+```
 
 
 
