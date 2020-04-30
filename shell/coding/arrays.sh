dd8c0b43909763fcd3574619ea1ff6fc30097921
doali <xou.xavl@gmail.com>
2020-04-30 15:35:52 +0200

    Arrays Sequences : - $(seq 1 10) - {1..10}

---
+++ shell/coding/arrays.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# repris de : https://www.linuxjournal.com/content/bash-arrays
+
+array=(one two three four [5]=five)
+
+echo "Array size: ${#array[*]}"
+
+echo "Array items:*"
+for item in ${array[*]}; do
+  printf "   %s\n" $item
+done
+
+echo "Array items:@"
+for item in ${array[@]}; do
+  printf "   %s\n" $item
+done
+
+echo "Array indexes:"
+for index in ${!array[*]}; do
+  printf "   %d\n" $index
+done
+
+echo "Array items and indexes:"
+for index in ${!array[*]}; do
+  printf "%4d: %s\n" $index ${array[$index]}
+done
+
+# ------------------------------------------------------------------------------
+# BE CAREFUL
+#Both syntax let us access all the values of the array and produce the same results, unless the expansion it's quoted. In this case a difference arises: in the first case, when using @, the expansion will result in a word for each element of the array. This becomes immediately clear when performing a for loop. As an example, imagine we have an array with two elements, "foo" and "bar"
+
+my_array=(foo bar)
+
+for i in "${my_array[@]}"; do echo "$i"; done # result on multiple lines
+
+for i in "${my_array[*]}"; do echo "$i"; done # result on a single line
---
+++ shell/coding/loop.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+for i in $(seq 1 5); do
+  #printf "%d" ${i}
+  echo ${i}
+done
+
+for i in {1..10}; do
+  #printf "%d" ${i}
+  echo ${i}
+done
