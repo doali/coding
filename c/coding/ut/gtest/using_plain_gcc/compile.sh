50c49c3a5a2fd842620fb8f9d85a8a115d3028ad
doali <xou.xavl@gmail.com>
2020-04-30 23:45:05 +0200

    Ajout du flag debug lors de la compilation : -g
    Scénario
    - gdb ./main (dans terminal)
    - b 8 (dans gdb)
    - r 1 456 78 (dans gdb)
    - refresh main.c (dans gdb)
    - n (dans gdb)
    - c (dans gdb)

--- c/coding/ut/gtest/using_plain_gcc/compile.sh
+++ c/coding/ut/gtest/using_plain_gcc/compile.sh
@@ -3,7 +3,8 @@
 readonly SRC_FILE="${1}"
 
 if [ ! -z ${SRC_FILE} ]; then
-  [ -f ${SRC_FILE} ] && gcc -o "${SRC_FILE%.*}" ${SRC_FILE}
+  # -g : or gdb
+  [ -f ${SRC_FILE} ] && gcc -g -o "${SRC_FILE%.*}" ${SRC_FILE}
 else
   echo "Usage ${BASH_SOURCE[0]} <file_name.c>"
 fi
