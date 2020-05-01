afdba0ef4bc295f0dabd0a86d44ab9d9d4f38c82
doali <xou.xavl@gmail.com>
2020-05-01 08:38:41 +0200

    Comments about building options

--- c/coding/ut/gtest/using_plain_gcc/compile_test.sh
+++ c/coding/ut/gtest/using_plain_gcc/compile_test.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
 g++ -o main_test main_test.cpp -L /usr/lib/ -I /usr/include/ -lgtest -lpthread
+
+#-L dir
+#    To specify directories for the linker to search for the library files.
+#-l library
+#    This specifies a library to link with.
+#-I dir
+#    This specifies a directories for the compile to search for when looking for include files.
