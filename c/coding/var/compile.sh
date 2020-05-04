c5f39a885281d6f5c3d5a5db3294cd2910123ecc
doali <xou.xavl@gmail.com>
2020-05-04 15:09:55 +0200

    Compile .c - checks if source file <source.c> exists - object file name is as following <source>

---
+++ c/coding/var/compile.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+usage() {
+  echo "Usage: $0 <source.c>"
+}
+
+# -ggdb : set on symbols for gdb
+[ ${#} = 1 ] && [ ! -z ${1} ] && [ -f ${1} ] && gcc -ggdb -o "${1%.*}" ${1} || usage
