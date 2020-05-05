64e2d8fcf4e30bed39e2bacdaccf1924c4f7d9aa
doali <xou.xavl@gmail.com>
2020-05-05 10:57:42 +0200

    Array.forEach Inner function

---
+++ js/coding/function/inner-func.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env nodejs
+
+/*
+ * Inner function
+ * Extra syntax to generate an array
+ * https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Op%C3%A9rateurs/Syntaxe_d%C3%A9composition
+ * - spread operator `...`
+ * https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/forEach
+ * - <Array>.forEach(([arg, ...]) => { statement; [statement, ...] }
+ */
+
+function f(y) {
+    return function(k) {
+        return y + k
+    }
+}
+
+let l = [...Array(10).keys()]; // spread operator
+
+let f_10 = f(10) //
+
+l.forEach(element => {
+    console.log(element + ":f_10(" + element + ")=" + f_10(element))
+})
