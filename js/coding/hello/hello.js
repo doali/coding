e7ffa94475e44439b9da95d89a5157a53b77e35a
doali <xou.xavl@gmail.com>
2020-05-05 09:40:57 +0200

    Format

--- js/coding/hello/hello.js
+++ js/coding/hello/hello.js
@@ -1,15 +1,11 @@
-
-var fac = function(n)
-{
-	if (n == 0) 
-	{
-		return 1
-	}
-	else
-	{
-		return n * fac (n - 1)
-	}
-}
+var fac =
+    function(n) {
+        if (n == 0) {
+            return 1
+        } else {
+            return n * fac(n - 1)
+        }
+    }
 
 console.log("fac(10)")
 console.log(fac(6))
