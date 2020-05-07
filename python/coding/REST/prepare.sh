1cee843253535ffc71d25b3352e659b9ebb1b716
doali <xou.xavl@gmail.com>
2020-05-07 12:37:55 +0200

    Flask !! - script : installation, lancement - scenario : utilisation - fichier : configuration, tests, ...

--- python/coding/REST/prepare.sh
+++ python/coding/REST/prepare.sh
@@ -1,6 +1,34 @@
+#!/usr/bin/env bash
+
+# ------------------------------------------------------------------------------
+# Preparation de l'environnement pour utiliser
+#- flask
+#- emettre des requetes 
+# ------------------------------------------------------------------------------
+
+# list des modules installés 
+# au niveau global
+#pip3 freeze
+
+# création d'un environnement virtuel python
+# nommé FLASK
 python3 -m venv FLASK
-pip3 freeze
+
+# Activation de l'environnement FLASK
 source FLASK/bin/activate
-pip3 freeze
+
+# Installation des modules pour
+# - flask
 pip3 install flask
 pip3 install requests
+
+# liste des modules installés
+# dans l'environnement virtuel
+#pip3 freeze
+
+# Sauvegarde
+#pip3 freeze >requirements_flask
+
+# Chargement, installation de modules
+# depuis un fichier d'export
+#pip3 -r requirements_flask
--- python/coding/REST/run_server.sh
+++ python/coding/REST/run_server.sh
@@ -1,1 +1,5 @@
+#!/usr/bin/env bash
+
+# Lancement du serveur
+# http://127.0.0.1:5000
 python3 server.py
