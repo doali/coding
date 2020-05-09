5ba2344fef18b9f7f26582e5f46927b520cf1819
doali <xou.xavl@gmail.com>
2020-05-09 21:29:25 +0200

    enum example

---
+++ c/coding/var/enum/enum_color.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum color {
+  BLACK = 0,
+  BROWN,
+  RED,
+  ORANGE,
+  YELLOW,
+  GREEN,
+  BLUE,
+  VIOLET,
+  GREY,
+  WHITE
+} r_color,
+    var_enum_color;
+// ATTENTION : r_color_var, var_enum_color sont des variables du type
+// enum color et non des alias vers le type enum color !!
+
+void r_color_var() {
+  //
+  // r_color c = BLACK; // lvalue est une VARIABLE !!
+  r_color = VIOLET;
+  var_enum_color = GREEN;
+  printf("val enum:%d\n", r_color);
+  printf("val enum:%d\n", var_enum_color);
+}
+
+const char *color_to_str(enum color e_color) {
+  static const char *conv[] = {"BLACK", "BROWN", "RED",    "ORANGE", "YELLOW",
+                               "GREEN", "BLUE",  "VIOLET", "GREY",   "WHITE"};
+  return conv[e_color];
+}
+
+int color_to_int(enum color first, enum color second) {
+  char code[2] = {'\0'};
+
+  sprintf(&code[0], "%d",
+          first); // itoa ne fonctionne pas... :-( => spintf(...)
+  sprintf(&code[1], "%d", second);
+
+  return atoi(code);
+}
+
+int compute_r(enum color first, enum color second, enum color third) {
+  printf("%d|%s\n", first, color_to_str(first));
+  printf("%d|%s\n", second, color_to_str(second));
+  printf("%d|%s\n", third, color_to_str(third));
+
+  int res = color_to_int(first, second);
+  printf("R|%d * 10^%d Ohm\n", res, third);
+
+  return res;
+}
+
+int main(void) {
+  compute_r(BLUE, GREEN, YELLOW);
+  r_color_var();
+}
