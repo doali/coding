b17e12ad2e7aad68fae1555a838004b53c7716a9
doali <xou.xavl@gmail.com>
2020-05-19 16:47:50 +0200

    static lib - ar -q <libname>.a <its_object_files>

---
+++ c/coding/experiments/static/operation.c
@@ -0,0 +1,9 @@
+#include "operation.h"
+
+int op_add(int a, int b) { return a + b; }
+
+extern int op_sub(int a, int b) { return a - b; }
+
+int op_mul(int a, int b) { return a * b; }
+
+int op_div(int a, int b) { return a / b; }
