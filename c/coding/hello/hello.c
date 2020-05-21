0650d5f99c3026ff4ebbea47abd791a9b9dd736d
doali <xou.xavl@gmail.com>
2020-05-21 22:01:25 +0200

    Refactoring

--- c/coding/hello/hello.c
+++ c/coding/hello/hello.c
@@ -44,16 +44,17 @@ void do_something() { // declaration + definition
 // ENTRY POINT
 // --------------------------------------------------------------------------------
 int main() { // declaration + definition
+
   LINE_BREAK
   printf("%d\n", __LINE__); // line number of the current file being compiled
   printf("%s\n", __FILE__); // name of the current file being compiled
   printf("%s\n", __DATE__); // current date
   printf("%s\n", __TIME__); // current time
+
   LINE_BREAK
-
   printf(__func__);
-  printf("\n");
 
+  LINE_BREAK
   do_something(); // call function
 
   return 0; // to ENV, i.e. to the OS
