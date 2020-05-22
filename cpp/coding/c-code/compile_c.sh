796549bca9545af5d640ee7fcfd5767c3aa827c2
doali <xou.xavl@gmail.com>
2020-05-22 03:12:06 +0200

    Stackoverflow extern C

--- cpp/coding/c-code/compile_c.sh
+++ cpp/coding/c-code/compile_c.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 # ------------------------------------------------------------------------------
+# https://www.geeksforgeeks.org/extern-c-in-c/
+# https://stackoverflow.com/questions/1041866/what-is-the-effect-of-extern-c-in-c
+# ------------------------------------------------------------------------------
 g++ -c ext-nothing.c # generate object file (.o) =>do_nothing(...)
 g++ -o C.exe C.cpp ext-nothing.o  # linked to executable C.exe
