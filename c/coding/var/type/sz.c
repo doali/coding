77332aa6984212581f50b53df526ab9a69d01963
doali <xou.xavl@gmail.com>
2020-05-24 10:28:48 +0200

    Preprocessor directives Types sizeof(...)

---
+++ c/coding/var/type/sz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PRINT_SZ(T, X) printf("%6s|%2lu byte(s)\n", T, sizeof(X));
+
+typedef struct {
+  char c;
+  short s;
+  int i;
+  char *p_c;
+  short *p_s;
+  int *p_i;
+} s_t;
+
+int main(int argc, char **argv) {
+  // -----------------------------------------------------------------------------
+  s_t s = {'c', 1, 1, NULL, NULL, NULL};
+
+  PRINT_SZ("char", s.c);
+  PRINT_SZ("short", s.s);
+  PRINT_SZ("int", s.i);
+  PRINT_SZ("char*", s.p_c);
+  PRINT_SZ("short*", s.p_s);
+  PRINT_SZ("int*", s.p_i);
+  // -----------------------------------------------------------------------------
+
+  return 0;
+}
