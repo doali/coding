fff844a10914aa2f20f208d03382e2f11fd3fe94
doali <xou.xavl@gmail.com>
2020-05-24 12:28:10 +0200

    Pointers Mem allocation / deallocation

--- c/coding/var/array/array-2.c
+++ c/coding/var/array/array-2.c
@@ -15,13 +15,13 @@
     printf("\n----------------------------------------------------------\n");
 
 /**
- * \fn int fetch(char *t, int r, int c) { return *(t + (c + r * COL)); }
+ * \fn char fetch(char *t, int r, int c) { return *(t + (c + r * COL)); }
  * \param t array of char
  * \param r row
  * \param c column
  * \return data in t[r][c]
  */
-int fetch(char *t, int r, int c) { return *(t + (c + r * COL)); }
+char fetch(char *t, int r, int c) { return *(t + (c + r * COL)); }
 
 int main(int argc, char **argv) {
 
@@ -43,6 +43,8 @@ int main(int argc, char **argv) {
   size_t sz_t_1 = sizeof(char) * ROW * COL;
   char *t_1 = (char *)malloc(sz_t_1);
   printf("%p:%lu (bytes)\n", t_1, sz_t_1);
+  // printf("%p:%lu (bytes)\n", t_1, sizeof(t_1)); //(!) 8 bytes (pointer)
+  // printf("%p:%lu (bytes)\n", t_1, sizeof(*t_1)); //(!) 1 byte (char)
   char *p = t_1;
   for (int i_r = 0; i_r < ROW; ++i_r) {
     for (int i_c = 0; i_c < COL; ++i_c) {
@@ -64,6 +66,42 @@ int main(int argc, char **argv) {
     free(t_1);
     t_1 = NULL;
   }
+
+  // -----------------------------------------------------------------------------
+  LINE_BREAK(1)
+  // -----------------------------------------------------------------------------
+  size_t sz_t_2_row = sizeof(char *) * ROW;
+  char **t_2 = (char **)malloc(sz_t_2_row); // sizeof(char*) * ROW
+                                            // sizeof(*t_2) * ROW
+  size_t sz_t_2_col = sizeof(char) * COL;
+  printf("%p:%lu (bytes)\n", t_2, sz_t_2_row + sz_t_2_col);
+
+  for (int i_r = 0; i_r < ROW; ++i_r) {
+    *(t_2 + i_r) = (char *)malloc(sizeof(char) * COL); // sizeof(char) * COL
+                                                       // sizeof(**t_2) * COL
+    // t_2[i_r] = (char *)malloc(sizeof(char) * COL);
+    for (int i_c = 0; i_c < COL; ++i_c) {
+      char *row = *(t_2 + i_r);
+      *(row + i_c) = i_r * COL + i_c;
+      // char *cell = (row + i_c); // OK
+      printf("|%p [%d]", &(*(*(t_2 + i_r) + i_c)), *(*(t_2 + i_r) + i_c));
+      // printf("|%p [%d]", cell, *cell); // OK
+    }
+    printf("|\n");
+  }
+
+  // first free columns
+  for (char i_r = 0; i_r < ROW; ++i_r) {
+    free(*(t_2 + i_r)); // free 1 ROW pointing to COL columns
+                        // => 1 * (sizeof(char) * COL)
+                        // => 1 * (1 byte * 2)
+    *(t_2 + i_r) = NULL;
+  } // 1 * (1 byte * 2) * (ROW = 4)
+    // => 8 bytes
+
+  // then free rows
+  free(t_2); // free 4 * 8 bytes
+  t_2 = NULL;
 
   return 0;
 }
---
+++ c/coding/var/array/array-3.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PRINT_SZ(T, X) printf("%16s|%4lu bytes\n", T, sizeof(X));
+#define PRINT_SZ_N(T, X, N)                                                    \
+  printf("%10s (x%2d)|%4lu bytes\n", T, N, sizeof(X) * N);
+
+#define LINE_BREAK printf("\n");
+#define LINE_BREAK_SECTION(TITLE)                                              \
+  printf(TITLE);                                                               \
+  printf("\n");
+#define LINE_BREAK_LINE printf("------------------------------------------\n");
+
+typedef struct {
+  char c;
+  short s;
+  int i;
+  char *p_c;
+  short *p_s;
+  int *p_i;
+} s_t;
+
+int main(int argc, char **argv) {
+  // -----------------------------------------------------------------------------
+  LINE_BREAK_LINE
+
+  s_t s = {'c', 1, 1, NULL, NULL, NULL};
+
+  PRINT_SZ("char", s.c);
+  PRINT_SZ("short", s.s);
+  PRINT_SZ("int", s.i);
+  PRINT_SZ("char*", s.p_c);
+  PRINT_SZ("short*", s.p_s);
+  PRINT_SZ("int*", s.p_i);
+
+  // -----------------------------------------------------------------------------
+
+  // -----------------------------------------------------------------------------
+  // ALLOCATION
+  // -----------------------------------------------------------------------------
+
+  LINE_BREAK_LINE
+
+  const u_int8_t ROW = 4;
+  const u_int8_t COL = 2;
+
+  short **matrix = (short **)malloc(sizeof(int *) * ROW);
+  PRINT_SZ("short**", matrix);
+  PRINT_SZ_N("short**", matrix, ROW);
+
+  LINE_BREAK
+  LINE_BREAK_SECTION("ALLOCATION")
+  LINE_BREAK
+
+  if (matrix != NULL) {
+    for (u_int8_t i_r = 0; i_r < ROW; ++i_r) {
+      matrix[i_r] = (short *)malloc(sizeof(**matrix) * COL);
+      //*(matrix + i_r) = (short*)malloc(sizeof(**matrix) * COL);
+      for (u_int8_t i_c = 0; i_c < COL; ++i_c) {
+        matrix[i_r][i_c] = i_r * COL + i_c;
+        printf("%p|[%d][%d]|%d\n", &matrix[i_r][i_c], i_r, i_c,
+               matrix[i_r][i_c]);
+      }
+      printf("\n");
+    }
+  }
+  printf("%p|%lu (bytes)\n", matrix,
+         (sizeof(**matrix) * COL * ROW) + (sizeof(int *) * ROW));
+
+  // -----------------------------------------------------------------------------
+  // FREE
+  // -----------------------------------------------------------------------------
+
+  LINE_BREAK_LINE
+  LINE_BREAK_SECTION("DEALLOCATION")
+  LINE_BREAK
+
+  // COL
+  printf("|%15s||%15s||%10s|\n", "(short**)", "(short*)", "(short)");
+  short **p_deallocator = matrix;
+  for (u_int8_t row = 0; row < ROW; ++row) {
+    printf("|%15p->%15p->%10d|\n", p_deallocator, *(p_deallocator + row) + 0,
+           *(*(p_deallocator + row) + 0));
+    printf("|%15p->%15p->%10d|\n", p_deallocator, *(p_deallocator + row) + 1,
+           *(*(p_deallocator + row) + 1)); // since 2 columns (COL = 2)
+
+    free(*(p_deallocator + row));  // free (short)
+    *(p_deallocator + row) = NULL; // INIT to NULL
+  }
+  // ROW
+  free(p_deallocator);  // free (short *)
+  p_deallocator = NULL; // INIT to NULL
+
+  LINE_BREAK_LINE
+
+  return 0;
+}
