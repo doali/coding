f6895b2366a8c2a6bca441166573c2f69d853b03
doali <xou.xavl@gmail.com>
2020-05-28 16:41:46 +0200

    Operator << and >>

--- c/coding/var/type/logic.c
+++ c/coding/var/type/logic.c
@@ -5,14 +5,29 @@
 #define LINE_BREAK printf("\n");
 
 typedef u_int8_t (*p_f_b)(u_int8_t, u_int8_t);
+typedef u_int16_t (*p_f_n)(u_int8_t, u_int8_t);
+
 typedef u_int8_t (*p_f_u)(u_int8_t);
 
 void op_b(const char *, p_f_b);
+
 void op_u(const char *, p_f_u);
+
+void op_shift(const char *, p_f_n, u_int16_t, u_int8_t);
 
 u_int8_t f_and(u_int8_t a, u_int8_t b) { return a & b; }
 u_int8_t f_or(u_int8_t a, u_int8_t b) { return a | b; }
 u_int8_t f_xor(u_int8_t a, u_int8_t b) { return a ^ b; }
+
+/**
+ * \return u_int16_t
+ */
+u_int16_t f_shift_l(u_int8_t a, u_int8_t n) { return a << n; }
+/**
+ * \return u_int16_t
+ */
+u_int16_t f_shift_r(u_int8_t a, u_int8_t n) { return a >> n; }
+
 u_int8_t f_cmp1(u_int8_t a) { return ~a; }
 u_int8_t f_cmp2(u_int8_t a) { return -a; }
 
@@ -26,14 +41,16 @@ int main(int argc, char **argv) {
   op_b("^", f_xor);
 
   LINE_BREAK
+  op_shift("<<", f_shift_l, 1, 8);
+
+  LINE_BREAK
+  op_shift(">>", f_shift_r, 128, 8);
+
+  LINE_BREAK
   op_u("~", f_cmp1);
 
   LINE_BREAK
   op_u("-", f_cmp2);
-
-  int a = 0;
-  int c = !a; // TODO
-  printf("%x %x\n", a, c);
 
   return 0;
 }
@@ -52,4 +69,11 @@ void op_b(const char *name, p_f_b f) {
   printf("%x %s %x | %7x |\n", 0, name, 1, f(0, 1));
   printf("%x %s %x | %7x |\n", 1, name, 0, f(1, 0));
   printf("%x %s %x | %7x |\n", 1, name, 1, f(1, 1));
+}
+
+void op_shift(const char *name, p_f_n f, u_int16_t start, u_int8_t n) {
+  printf("n | e = %3d %s n    |\n", start, name);
+  printf("--|-----------------|\n");
+  for (u_int8_t shift = 0; shift <= n; ++shift)
+    printf("%1x | %16d|\n", shift, f(start, shift));
 }
