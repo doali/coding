cc686df4d1efbf3e4cdd41e576838e3660a0e8b4
doali <xou.xavl@gmail.com>
2020-12-27 23:29:47 +0000

    Linux, kernel, man pages - des liens intéressants !!

--- c/README.md
+++ c/README.md
@@ -1,1 +1,6 @@
 Coding in C
+
+## Linux
+
+- [TLPI, The Linux Programming Interface](https://man7.org/tlpi/)
+- [glibc](https://www.gnu.org/software/libc/)
--- ruby/coding/net/raw/client.rb
+++ ruby/coding/net/raw/client.rb
@@ -4,6 +4,12 @@
 # A explorer !!
 # https://github.com/todb/packetfu
 # --------------------------------------------------------------------------------
+# Autre chose aussi
+# gem install racket
+# --------------------------------------------------------------------------------
+# Pour comprendre
+# https://medium.com/geckoboard-under-the-hood/how-to-build-a-network-stack-in-ruby-f73aeb1b661b
+# --------------------------------------------------------------------------------
 
 require 'socket'
 
