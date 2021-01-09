a54b9d3ad3712fed51ca7aebc9efc9cb3313d9fc
doali <xou.xavl@gmail.com>
2021-01-09 10:02:44 +0100

    POC : select avec socket server UDP - client ruby UDP - client nc UDP - traces tcpdump

--- ruby/coding/net/select/ex_1.rb
+++ ruby/coding/net/select/ex_1.rb
@@ -2,7 +2,9 @@
 
 require 'socket'
 
-mysocket = TCPSocket.new('127.0.0.1', 6666)
+mysocket = UDPSocket.new
+#mysocket.bind('192.168.1.87', 6666)
+mysocket.bind('192.168.1.87', 6666)
 
 th = Thread.new do
     while true
@@ -18,6 +20,8 @@ th = Thread.new do
                     exit
                 else
                     puts "Received a message"
+                    puts "#{buf.unpack("C*")}"
+                    puts "#{buf}"
                 end
             end
         end
@@ -25,3 +29,26 @@ th = Thread.new do
     end
     puts "DEBUG : loop >>"
 end
+
+status = th.status
+puts "status #{status}"
+
+cpt = 0
+Thread.new {
+  loop do
+    sleep 1
+    cpt += 1
+    puts cpt
+  end
+}
+
+
+sleep(120)
+
+# Liens utiles
+# --------------------------------------------------------------------------------
+# https://dzone.com/articles/tcpdump-learning-how-read-udp
+# https://dzone.com/articles/tcpdump-learning-how-read-udp
+# https://help.ubidots.com/en/articles/937233-sending-tcp-udp-packets-using-netcat
+# https://leejava.wordpress.com/2009/08/24/udp-client-and-server-in-ruby/
+# https://stackoverflow.com/questions/6165735/understanding-io-select-when-reading-socket-in-ruby
