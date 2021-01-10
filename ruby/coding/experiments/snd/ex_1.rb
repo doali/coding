66e8094d78c4177437b459a6aa38df5d1dc5392c
doali <xou.xavl@gmail.com>
2021-01-10 19:08:41 +0100

    Refactoring... - ajout de la saisie d'un argument en ligne de commande - vérification de l'existence du chemin passé

--- ruby/coding/experiments/snd/ex_1.rb
+++ ruby/coding/experiments/snd/ex_1.rb
@@ -2,29 +2,42 @@
 
 require 'ruby2d'
 
-path_dir = '/home/blackpc/Music/Artist/TheRollingStones/Hot Rocks, 1964-1971 (1 of 2)'
+# Si un argument est passé en ligne de commande ARGV[0]
+# Alors on joue depuis ce répertoire sinon => après le ||
+path_dir = ARGV[0] || '/home/blackpc/Music/Artist/TheRollingStones/Hot Rocks, 1964-1971 (1 of 2)'
 
-song = nil
-tt = nil
+# Vérification de l'existence du chemin
+exit(1) if not File.directory?(path_dir) 
 
-pool_th = []
-
+# Timer de l'application
 timer_app = Thread.new {
     sleep 10
     puts "Finished"
 }
 
+# File d'exécution séparé
 songs_app = Thread.new {
+    # Tous les noms des fichiers contenus dans <path_dir>
     songs = Dir.entries(path_dir).select { |f| File.file? File.join(path_dir, f) }    
+    
+    # Pour chaque nom de fichier
     songs.each do |song|
         p_song = path_dir + "/" + song
-        curr_song = Music.new(p_song)
+
+        curr_song = Music.new(p_song) # <== ICI utilisation de ruby2d
+
+        # Arme un timer pour écourter la chason
         timer_song = Thread.new {
             sleep 3                
             curr_song.stop                
         }
         puts song
-        curr_song.play
+
+        curr_song.play # <== ICI utilisation de ruby2d
+
+        # Attente de la fin du thread timer_song
+        # RM : si le thread timer_app termine avant 
+        # => on quitte l'application
         timer_song.join
     end
 }
