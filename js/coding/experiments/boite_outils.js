c9291f93cdc3fde12e5cc06778c934ea8a9b27df
blackpc <xou.xavl@gmail.com>
2021-02-14 22:00:35 +0100

    Differentes façons de déclarer

---
+++ js/coding/experiments/boite_outils.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var boite_outils = {}
+
+boite_outils.clou = 11;
+boite_outils.marteau = true;
+boite_outils.sandwich = "thon";
+
+console.log(boite_outils);
+
+let modern_tool_box = {
+	clou: 11,
+	marteau: true,
+	sandwich: "thon"
+}
+
+console.log(modern_tool_box)
