466e1166809407bfde728cf9a057faf6eeebe78c
blackpc <xou.xavl@gmail.com>
2021-02-25 22:39:49 +0100

    Tests glib

---
+++ c/coding/experiments/glib/ex_1.c
@@ -0,0 +1,30 @@
+// --------------------------------------------------------------------------------
+// https://developer.ibm.com/tutorials/l-glib/
+// --------------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <glib.h>
+
+int main(int argc, char** argv) {
+     GList* list = NULL;
+
+     list = g_list_append(list, "Hello world!");
+     list = g_list_prepend(list, "titi_1");
+     list = g_list_prepend(list, "titi_2");
+     list = g_list_prepend(list, "titi_3");
+     list = g_list_prepend(list, "titi_4");
+
+     struct s {
+          int val;
+          const char* name;
+     } inst_struct = {10, "id_0"};
+
+     list = g_list_prepend(list, &inst_struct);
+
+     int v = ((struct s*)(g_list_first(list)->data))->val;
+     const char* id = ((struct s*)(g_list_first(list)->data))->name;
+     
+     printf("%d | %s\n", v, id);
+
+     return 0;
+}
