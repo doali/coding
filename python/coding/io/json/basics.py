832ca7614e94d863f6243256e9b93cfc4ffc1188
blackpc <xou.xavl@gmail.com>
2021-03-11 00:20:16 +0100

    Validateur - contraintes exprimees dans constraintes.json - basics.js lance la validation

--- python/coding/io/json/basics.py
+++ python/coding/io/json/basics.py
@@ -1,60 +1,68 @@
 #!/usr/bin/env python3
 
+
 import json
 
-obj_1 = json.load(open("comp_1.json", "r"))
-obj_2 = json.load(open("comp_2.json", "r"))
-obj_3 = json.load(open("comp_3.json", "r"))
 
-obj_rules = json.load(open("data.json", "r"))
+obj_rules = json.load(open("constraints.json", "r"))
 
-dico = {
-    "component_1" : obj_1,
-    "component_2" : obj_2,
-    "component_3" : obj_3,
-}
+def get_path(comp_name):
+    for k in obj_rules["components"]:
+        if k['name'] == comp_name:
+            return k['path']
+    return None
 
-def check_rule(field_1, field_2, rule):
-    if (rule == 'equal'):
-        return field_1 == field_2
-    elif (rule == 'different'):
-        return field_1 != field_2
-    else:
-        return false
 
-lst_comp = obj_rules['components']
-print(lst_comp)
+def fill_dico():
+    dico = {}
 
-rule_comp_1 = lst_comp[0]
-obj_comp_1 = dico[rule_comp_1['name']]
-print(obj_comp_1)
-obj_comp_1_depends = rule_comp_1['depends']
-print(obj_comp_1_depends)
+    for comp in obj_rules["components"]:
+        dico[comp["name"]] = json.load(open(comp["path"], "r"))
 
-const_1_2 = obj_comp_1_depends[0]
-obj_comp_2 = dico[const_1_2['name']]
-print(obj_comp_2)
+    return dico
 
-for k, v in const_1_2['constraint'].items():
-    print(f"{k} => {v}")
-    res = check_rule(obj_1[k], obj_comp_2[k], v)
-    print("RES:" + str(res))
 
-# for comp in obj_comp_1_depends:
-#     obj = comp
+def is_atomic_rule_satisfied(field_1, field_2, rule):    
+    switcher = {
+        "equal" : (field_1 == field_2),
+        "different" : (field_1 != field_2)
+    }
+
+    res = switcher.get(rule, False)
+
+    if (not res):
+        print(f"KO:{rule} \n{field_1:>30} \n{field_2:>30}")
+
+    return res
+
+
+def check_constraints(dico):
+    all_satisfied = True
+    res = True
+
+    for comp in obj_rules["components"]:
+
+        for dependence in comp["depends"]:
+            obj_comp_2 = dico[dependence["name"]]
+
+            for k, v in dependence["constraint"].items():
+                if k in obj_comp_2:
+                    res = is_atomic_rule_satisfied(dico[comp["name"]][k], obj_comp_2[k], v)
+                    all_satisfied = all_satisfied and res
+                    if not res:
+                        print(f"{comp['path']} => {get_path(dependence['name'])}")
+                        print("---")
+                else:
+                    if 'target' in v:
+                        res = is_atomic_rule_satisfied(dico[comp["name"]][k], obj_comp_2[v["target"]], v['op'])
+                        all_satisfied = all_satisfied and res
+                        if not res:
+                            print(f"{comp['path']} => {get_path(dependence['name'])}")
+                            print("---")
+                            
+    return all_satisfied and res
 
 
 if __name__ == "__main__":
-    pass
-    print(obj_1)
-    print(obj_2)
-    print(obj_3)
-    print(obj_rules)
-    res = check_rule('titi', 'toto', 'different')
-    print(res)
-    res = check_rule('titi', 'toto', 'equal')
-    print(res)
-    res = check_rule('titi', 'titi', 'equal')
-    print(res)
-    res = check_rule('titi', 'titi', 'different')
-    print(res)    
+    dico = fill_dico()
+    print("ok") if (check_constraints(dico)) else "ko"
