45b42205c0e451be37e38f2b891f4ee252ad3a38
doali <xou.xavl@gmail.com>
2021-03-14 08:00:12 +0000

    Ajout de flat => list

--- python/coding/io/json/checker.py
+++ python/coding/io/json/checker.py
@@ -62,7 +62,44 @@ def check_constraints(dico):
                             
     return all_satisfied and res
 
+def is_atomic_rule_obj_satisfied(obj_1, obj_2, pattern_rule):
+    for e_pattern in pattern_rule:
+        res = is_atomic_rule_satisfied(obj_1[e_pattern], obj_2[e_pattern], pattern_rule[e_pattern])
+        if not res:
+            break
+
+    return res
+
+
+def is_flat_to_list_satisfied(obj_flat, obj_list, pattern):
+    # Ajouter dans constraints.json 
+    # - un moyen d'indiquer la liste : target = list, name = clef_liste    
+    # Algo
+    result = False
+    
+    obj_flat_restricted = flat_to_obj(obj_flat, pattern)
+
+    for e_list in obj_list:
+        print(e_list)
+        
+        result = is_atomic_rule_obj_satisfied(obj_flat_restricted, e_list, pattern)
+        if result:
+            # Au moins un element e_list satisfait la regle (pattern)
+            break
+
+    return result
+
+
+def flat_to_obj(obj_flat, pattern):
+    obj = {}
+
+    for e_pattern in pattern:
+        obj[e_pattern] = obj_flat[e_pattern]
+    
+    return obj
+
 
 if __name__ == "__main__":
     dico = fill_dico()
     print("ok") if (check_constraints(dico)) else "ko"
+    is_flat_to_list_satisfied(dico['component_1'], dico['component_2'], { "field_list_1_1" : "equal", "field_list_1_2" : "equal"})
