65da63c8c8e81a24babb74284ce65db67fa3ba3b
doali <xou.xavl@gmail.com>
2021-03-22 21:39:00 +0100

    python2 => python3

--- python/coding/experiments/regex_mail.py
+++ python/coding/experiments/regex_mail.py
@@ -3,21 +3,22 @@
 import re
 import sys
 
+
 def check_addr_mail(addr_mail):
     reg_ex = re.compile(r"(\w+)@(\w+\.[a-zA-Z]{2,3})")
     tuple_groupe = reg_ex.search(addr_mail)
     addr = False
-    if (tuple_groupe <> None):
-        print tuple_groupe.group(0)
-        print tuple_groupe.group(1)
-        print tuple_groupe.group(2)
+    if tuple_groupe != None:
+        print(tuple_groupe.group(0))
+        print(tuple_groupe.group(1))
+        print(tuple_groupe.group(2))
         if len(tuple_groupe.group(0)) == len(addr_mail):
             addr = True
 
     if not addr:
-        print addr_mail,'is not valid'
+        print(f"{addr_mail}, is not valid")
     else:
-        print addr_mail,'is valid'
+        print(f"{addr_mail}, is valid")
 
     return tuple_groupe
 
@@ -26,7 +27,8 @@ def main():
     if len(sys.argv) == 2:
         check_addr_mail(sys.argv[1])
     else:
-        print 'Usage : %s <user>@<domain>' % (sys.argv[0])
+        print(f"Usage : {sys.argv[0]} <user>@<domain>")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
