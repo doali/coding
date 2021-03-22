bbc7fdf51619fc1d72a99ad0d82dd711ffed5f32
doali <xou.xavl@gmail.com>
2021-03-22 21:16:09 +0100

    Python2 => Python3

--- python/coding/experiments/nombre_cache.py
+++ python/coding/experiments/nombre_cache.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 
-CLEF=3
+CLEF = 3
 MAX_ESSAI = 10
 
-print 'Essayez de trouver le nombre cache'
+print("Essayez de trouver le nombre cache")
 
 resultat = 0
 nb_essai = 0
 
 while resultat != CLEF and nb_essai < MAX_ESSAI:
-    resultat = int(raw_input('>'))
+    resultat = int(input(">"))
     nb_essai = nb_essai + 1
 
 if resultat == CLEF:
-    print('Bravo')
+    print("Bravo")
 else:
-    print('Vous pouvez toujours rejouer !')
-
+    print("Vous pouvez toujours rejouer !")
