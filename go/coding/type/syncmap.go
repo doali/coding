b7bbda341570382c8851cfec39a4321257f4c619
doali <xou.xavl@gmail.com>
2021-03-31 22:12:08 +0200

    Suppression code doublon

--- go/coding/type/syncmap.go
+++ go/coding/type/syncmap.go
@@ -42,31 +42,3 @@ func syncMapUsage() {
 
 	fmt.Println("---------------------------")
 }
-
-func syncMapPerson() {
-	fmt.Println("sync.Map test (Go 1.9+ only)")
-	fmt.Println("----------------------------")
-
-	// Create the threadsafe map.
-	var sm sync.Map
-
-	// Fetch an item that doesn't exist yet.
-	result, ok := sm.Load("hello")
-	if ok {
-		fmt.Println(result.(string))
-	} else {
-		fmt.Println("value not found for key: `hello`")
-	}
-
-	// Store an item in the map.
-	sm.Store("hello", "world")
-	fmt.Println("added value: `world` for key: `hello`")
-
-	// Fetch the item we just stored.
-	result, ok = sm.Load("hello")
-	if ok {
-		fmt.Printf("result: `%s` found for key: `hello`\n", result.(string))
-	}
-
-	fmt.Println("---------------------------")
-}
