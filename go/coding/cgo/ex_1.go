5d92393561578caf14f1aa7158c8bd846396e503
doali <xou.xavl@gmail.com>
2021-04-12 11:32:39 +0000

    Appel à une fonction de la lib - FONCTIONNE
    ATTENTION !!
    - il faut déclarer JUSTE AVANT le import C
      - PAS de ligne VIDE

--- go/coding/cgo/ex_1.go
+++ go/coding/cgo/ex_1.go
@@ -1,44 +1,11 @@
 package main
 
-// #cgo LDFLAGS: -L//workspace/coding/go/coding/cgo/operation -loperation
-
+// #cgo LDFLAGS: -L/workspace/coding/go/coding/cgo/operation -loperation
 // #include "operation/operation.h"
-// typedef int (*intFunc) ();
-//
-// int
-// bridge_int_func(intFunc f)
-// {
-//		return f();
-// }
-//
-// int fortytwo()
-// {
-//	    return 42;
-// }
-/*
-int sum(int a, int b)
-{
-	return a + b;
-}
-
-int mul(int a, int b)
-{
-	return a * b;
-}
-// typedef int (*p_op)(int, int); // https://golang.org/cmd/cgo/ PAS DE POINTEUR SUR FONCTION
-*/
 import "C"
 import "fmt"
 
 func main() {
-	f := C.intFunc(C.fortytwo)	
-	fmt.Println(int(C.bridge_int_func(f)))
-	// Output: 42
-	resMul := C.mul(2, 3)
-	fmt.Println("resMul:", resMul)
-	resSum := C.sum(2, 3)
-	fmt.Println("resMul:", resSum)
-	
 	resLibOpAdd := C.op_add(10, 20)
 	fmt.Println("resLibOpAdd:", resLibOpAdd)	
 }
