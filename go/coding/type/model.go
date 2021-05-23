baf204dccabfbc395f75da3300b0eca60c3473bb
doali <doali@domain.com>
2021-05-23 11:18:43 +0200

    Construction allocation

--- go/coding/type/model.go
+++ go/coding/type/model.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 )
 
@@ -19,4 +20,22 @@ func newPerson(name string, id uint64) *Person {
 
 func (p *Person) String() string {
 	return "{" + p.name + ":" + strconv.FormatUint(p.id, 10) + "}"
+}
+
+func main() {
+	var p1 *Person
+	p1 = newPerson("Titi", 64)
+	fmt.Println(p1.String())
+
+	var p2 Person
+	p2 = *p1
+	fmt.Println(p2.String())
+
+	var p3 *Person = &Person{"Miaou", 65}
+	fmt.Println(p3)
+
+	var p4 *Person = new(Person)
+	p4.id = 62
+	p4.name = "Riri"
+	fmt.Println(p4)
 }
