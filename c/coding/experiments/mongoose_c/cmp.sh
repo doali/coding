7cc9209905e1cd50b8e33aecfc61702110cbf14c
doali <xou.xavl@gmail.com>
2021-06-24 09:28:22 +0000

    mongoose

---
+++ c/coding/experiments/mongoose_c/cmp.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+gcc -o mini_http_server mini_http_server.c mongoose.c 
---
+++ c/coding/experiments/mongoose_c/mini_http_server.c
@@ -0,0 +1,18 @@
+#include "mongoose.h"
+
+static const char *s_web_root_dir = ".";
+static const char *s_listening_address = "http://localhost:8000";
+
+static void cb(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
+  struct mg_http_serve_opts opts = {.root_dir = s_web_root_dir};
+  if (ev == MG_EV_HTTP_MSG) mg_http_serve_dir(c, ev_data, &opts);
+}
+
+int main(int argc, char *argv[]) {
+  struct mg_mgr mgr;
+  mg_mgr_init(&mgr);
+  mg_http_listen(&mgr, s_listening_address, cb, &mgr);
+  for (;;) mg_mgr_poll(&mgr, 1000);
+  mg_mgr_free(&mgr);
+  return 0;
+}
