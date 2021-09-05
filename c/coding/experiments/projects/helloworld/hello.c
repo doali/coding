ccb385dc0ae3dc39ff34391808fd03b0140ec6cf
doali <doali@domain.com>
2021-09-05 15:35:00 +0200

    Formatage stdout

--- c/coding/experiments/projects/helloworld/hello.c
+++ c/coding/experiments/projects/helloworld/hello.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
+#include <string.h> // strlen
 
-void str2ascii(char* msg) {
-  if (msg == NULL) {
+void str2ascii(char *msg)
+{
+  if (msg == NULL)
+  {
     return;
   }
 
   char *p_msg = msg;
-  while (*p_msg != '\0') {
-    printf("%c|%d\n", *p_msg, *p_msg);
+  while (*p_msg != '\0')
+  {
+    printf("|%c|%3d|\n", *p_msg, *p_msg);
     ++p_msg;
   }
 }
 
-int main(int argn, char **argv) {
+int str2ascii_(const char *str)
+{
+  if (str == NULL)
+    return -1;
+  size_t sz = strlen(str);
+  for (int index = 0; index < sz; index++)
+  {
+    printf("|%3d|%c|\n", str[index], str[index]);
+  }
+  return 0;
+}
+
+int main(int argn, char **argv)
+{
   int index = argn - 1;
-  do {
+  do
+  {
     printf("[%d]:%s\n", index, argv[index]);
     str2ascii(argv[index]);
     --index;
