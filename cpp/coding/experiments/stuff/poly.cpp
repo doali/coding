00795ee24310c10150a9ae44868f7e0a37b8615d
xavL <xou.xavl@gmail.com>
2021-12-01 12:13:04 +0100

    MAJ : virtual

--- cpp/coding/experiments/stuff/poly.cpp
+++ cpp/coding/experiments/stuff/poly.cpp
@@ -2,51 +2,37 @@
 #include <string>
 #include <vector>
 
-void f(int *& p_r) {
+void f(int *&p_r) {
   std::cout << p_r << std::endl;
   std::cout << *p_r << std::endl;
 }
 
-class A
-{
+class A {
 public:
   std::string label;
   A(const std::string _a) : label(_a) {}
-  friend std::ostream& operator<<(std::ostream& os, const A& _a)
-  {
+  friend std::ostream &operator<<(std::ostream &os, const A &_a) {
     os << _a.label;
     return os;
   }
-  virtual ~A()
-  {
-    std::cout << *this << " is about to be free" << std::endl;
-  }
+  virtual ~A() { std::cout << *this << " is about to be free" << std::endl; }
 
-  virtual void doSomething()
-  {
+  virtual void doSomething() {
     std::cout << *this << " is walking." << std::endl;
   }
 };
 
-class B : public A
-{
+class B : public A {
 public:
   std::string more;
   B(const std::string _a, const std::string _b) : A(_a), more(_b) {}
-  ~B()
-  {
-    std::cout << *this << " subclass" << std::endl;
-  }
-  friend std::ostream& operator<<(std::ostream& os, const B& _b)
-  {
+  virtual ~B() { std::cout << *this << " subclass (being free)" << std::endl; }
+  friend std::ostream &operator<<(std::ostream &os, const B &_b) {
     os << _b.label << " | " << _b.more;
     return os;
   }
 
-  void doSomething()
-  {
-    std::cout << *this << " is running." << std::endl;
-  }
+  void doSomething() { std::cout << *this << " is running." << std::endl; }
 };
 
 int main(void) {
@@ -60,32 +46,31 @@ int main(void) {
 
   A a0("a0");
 
-  A* a1 = new A("a1");
+  A *a1 = new A("a1");
   std::cout << *a1 << std::endl;
 
-  A* a2 = new A("a2");
+  A *a2 = new A("a2");
   std::cout << *a2 << std::endl;
 
-  A& a3 = *a2;
+  A &a3 = *a2;
 
   a2->label = "a2_";
 
   std::cout << a3 << std::endl;
 
-  delete(a1);
-  delete(a2);
+  delete (a1);
+  delete (a2);
 
   std::cout << B("b", "more") << std::endl;
 
-  std::vector<A*> v;
+  std::vector<A *> v;
   A la = A("_titi");
   B lb = B("_titi", "titou");
 
   v.push_back(&la);
   v.push_back(&lb);
 
-  for (auto e: v)
-  {
+  for (auto e : v) {
     e->doSomething();
   }
 
