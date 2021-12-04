24ade984f18c8a0b6600d4c8821c5460bc864bb9
xavl <xavl-doali@gmail.com>
2021-12-04 22:45:49 +0100

    foreach in cpp

---
+++ cpp/coding/type/vector/vector.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+// Code récupéré et étendu
+
+int main()
+{
+  std::vector<int> const tableau{-1, 28, 346};
+  // Déclaration explicite d'un itérateur de std::vector d'entiers.
+  
+  std::vector<int>::const_iterator debut_tableau{std::begin(tableau)};
+  
+  // Déclaration plus courte en utilisant auto.
+  auto fin_tableau{std::end(tableau)};
+
+  // Good way
+  for (auto const &e : tableau)
+    std::cout << e << std::endl;
+
+  // Old school...
+  std::vector<int>::const_iterator it;
+  for (it = tableau.begin(); it != tableau.end(); ++it) {
+    std::cout << *it << std::endl;
+  }
+
+  // Very old school...
+  for (int i = 0; i < tableau.size(); ++i) {
+    std::cout << tableau[i] << std::endl;
+  }
+
+  return 0;
+}
