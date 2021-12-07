291590860a5eebb7c1391ab7cfe27e862b873bc7
xavl <xavl-doali@gmail.com>
2021-12-07 10:30:17 +0100

    SLOT personnalisé

--- cpp/coding/projQt/oc_cpp_qt/qmake_empty/test/fenetre.cpp
+++ cpp/coding/projQt/oc_cpp_qt/qmake_empty/test/fenetre.cpp
@@ -4,17 +4,18 @@
 Fenetre::Fenetre(unsigned int const largeur /* = 300 */, unsigned int const hauteur /* = 200 */) :
     _bouton(new QPushButton("OK", this)),
     _slider(new QSlider(Qt::Horizontal, this)),
-    _lcd(new QLCDNumber(this)),
+    _bar(new QProgressBar(this)),
     _layout(new QHBoxLayout(this))
 {
     setLayout(_layout);
     _layout->addWidget(_slider);
-    _layout->addWidget(_lcd);
+    _layout->addWidget(_bar);
     _layout->addWidget(_bouton);
     _bouton->setFont(QFont("Comic Sans MS", 14));
-    _lcd->setSegmentStyle(QLCDNumber::Flat);
-    QObject::connect(_bouton, SIGNAL(clicked()), qApp, SLOT(quit()));
-    QObject::connect(_slider, SIGNAL(valueChanged(int)), _lcd, SLOT(display(int)));
+    _bar->setRange(0, 100);
+    QObject::connect(_bouton, SIGNAL(clicked()), this, SLOT(resetSlider()));
+    QObject::connect(_bouton, SIGNAL(clicked()), _bar, SLOT(reset()));
+    QObject::connect(_slider, SIGNAL(valueChanged(int)), _bar, SLOT(setValue(int)));
 }
 
 Fenetre::~Fenetre()
@@ -22,4 +23,9 @@ Fenetre::~Fenetre()
     // Tous les composants ont pour parent Fenetre
     // - Qt libère la mémoire pour les fils de Fenetre
     // => aucun delete à réaliser dans ce cas
+}
+
+void Fenetre::resetSlider()
+{
+    _slider->setValue(0);
 }
--- cpp/coding/projQt/oc_cpp_qt/qmake_empty/test/fenetre.h
+++ cpp/coding/projQt/oc_cpp_qt/qmake_empty/test/fenetre.h
@@ -4,18 +4,21 @@
 #include <QWidget>
 #include <QPushButton>
 #include <QSlider>
-#include <QLCDNumber>
+#include <QProgressBar>
 #include <QLayout>
 
 class Fenetre : public QWidget
 {    
+    Q_OBJECT
 public:    
     Fenetre(unsigned int const largeur = 300, unsigned int const hauteur = 200);
     virtual ~Fenetre();
+public slots:
+    void resetSlider();
 private:
     QPushButton* _bouton;
     QSlider* _slider;
-    QLCDNumber* _lcd;
+    QProgressBar* _bar;
     QLayout* _layout;
 };
 
