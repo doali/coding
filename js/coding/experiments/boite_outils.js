f943096fde7576f2172b71705da877ed25cb6323
doali <doali@gmail.com>
2022-09-04 09:45:45 +0200

    Other ways to create object

--- js/coding/experiments/boite_outils.js
+++ js/coding/experiments/boite_outils.js
@@ -15,3 +15,23 @@ let modern_tool_box = {
 }
 
 console.log(modern_tool_box)
+
+function cons_boite_outils(nb_clou, marteau_present, sandwich) {
+	this.clou = nb_clou;
+	this.marteau = marteau_present;
+	this.sandwich = sandwich;
+}
+
+let func_cons_boite = new cons_boite_outils(11, true, "thon")
+console.log(func_cons_boite)
+
+class BoiteOutils {
+	constructor(clou, marteau, sandwich) {
+		this.clou = clou;
+		this.marteau = marteau;
+		this.sandwich = sandwich
+	}
+}
+
+let class_BoiteOutils = new BoiteOutils(11, true, "thon")
+console.log(class_BoiteOutils)
