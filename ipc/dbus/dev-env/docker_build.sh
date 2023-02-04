7fc3f982b103eaa72a2a60ba1b6cfdd775a21786
doali <doali@gmail.com>
2023-02-04 22:26:25 +0100

    Commit without using cache

--- ipc/dbus/dev-env/docker_build.sh
+++ ipc/dbus/dev-env/docker_build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-docker build --no-cache -t doali/dev-cpp:0.0.1 --build-arg UID=$(id -u) --build-arg PASSWORD=titi .
+#docker build --no-cache -t doali/dev-cpp:0.0.1 --build-arg UID=$(id -u) --build-arg PASSWORD=titi .
+docker build -t doali/dev-cpp:0.0.1 --build-arg UID=$(id -u) --build-arg PASSWORD=titi .
