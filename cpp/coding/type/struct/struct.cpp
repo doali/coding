4e2e68c76ec65b21176e1c150f0ae6739048ac57
doali <xou.xavl@gmail.com>
2023-03-27 11:06:13 +0200

    comments

--- cpp/coding/type/struct/struct.cpp
+++ cpp/coding/type/struct/struct.cpp
@@ -1,30 +1,38 @@
 #include <iostream>
 
-struct _titi
+namespace // anonymous namespace (visibility limited to this file) (similar to "static" keyword for C language regarding visibility)
 {
-	int x{1};
-	int y;
-} titi{
-	3, 4};
+	struct _titi
+	{
+		int x{1}; // initialization
+		int y;
+	} titi{// variable
+		   3,
+		   4};
 
-using s_titi = struct _titi;
-typedef struct _titi oldschool_titi;
+	using s_titi = struct _titi; // new way for typedef
+	typedef struct _titi oldschool_titi;
 
-std::ostream &operator<<(std::ostream &os, const struct _titi &element)
-{
-	return os << "{" << element.x << ", " << element.y << "}";
+	std::ostream &operator<<(std::ostream &os, const struct _titi &element)
+	{
+		return os << "{" << element.x << ", " << element.y << "}";
+	}
 }
 
 int main(void)
 {
-	struct _titi t = {1, 2};
-	std::cout << t << std::endl;
-	std::cout << titi << std::endl;
+	std::cout << titi << std::endl; // variable !!
 
-	s_titi s_t;
+	struct _titi t_0 = {1, 2};
+	std::cout << t_0 << std::endl;
+
+	_titi t_1 = {1, 2}; // struct keyword optional
+	std::cout << t_1 << std::endl;
+
+	s_titi s_t; // thanks to using
 	std::cout << s_t << std::endl;
 
-	oldschool_titi o_t;
+	oldschool_titi o_t; // thanks to typedef
 	std::cout << o_t << std::endl;
 
 	exit(0);
