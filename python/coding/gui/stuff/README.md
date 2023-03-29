cd9ee373d0cabd16f615b39850aac537fe196dc7
doali <xou.xavl@gmail.com>
2023-03-29 10:02:43 +0200

    python qt - sandbox geometry

--- python/coding/gui/stuff/README.md
+++ python/coding/gui/stuff/README.md
@@ -1,3 +1,13 @@
 # Stuff
 
 _Répertoire `stuff` à supprimer..._
+
+# Install
+
+```bash
+apt install python3-pyqt5
+```
+
+# Biblio
+
+- [pythonguis](https://www.pythonguis.com/tutorials/bitmap-graphics/)
---
+++ python/coding/gui/stuff/qt_pythonguis.py
@@ -0,0 +1,44 @@
+import sys
+from math import *
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtCore import Qt
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.label = QtWidgets.QLabel()
+        canvas = QtGui.QPixmap(400, 300)
+        canvas.fill(Qt.white)
+        self.label.setPixmap(canvas)
+        self.setCentralWidget(self.label)
+        # self.draw_line()
+        # self.draw_point()
+        self.draw_cirlce()
+
+    def draw_line(self):
+        painter = QtGui.QPainter(self.label.pixmap())
+        painter.drawLine(10, 10, 300, 200)
+        painter.end()
+
+    def draw_point(self):
+        painter = QtGui.QPainter(self.label.pixmap())
+        painter.drawPoint(50, 50)
+        painter.drawPoint(floor(50 + 4 * cos(pi / 4)), floor(50 + 4 * cos(pi / 4)))
+        painter.end()
+
+    def draw_cirlce(self):
+        painter = QtGui.QPainter(self.label.pixmap())
+        val = [x / (2 * pi) for x in range(1000)]
+        for angle in val:
+            painter.drawPoint(
+                floor(150 + 50 * cos(angle)), floor(150 + 50 * sin(angle))
+            )
+        painter.end()
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec_()
