a3317e10c847309776ffbed83994d9b76b8334f7
doali <xou.xavl@gmail.com>
2023-03-31 18:17:35 +0200

    print, animation, triangle...

--- c/coding/experiments/ncurses/ncurses_animation.c
+++ c/coding/experiments/ncurses/ncurses_animation.c
@@ -14,23 +14,31 @@ static void draw_point(unsigned int x, unsigned int y)
 	addch('#');
 }
 
+static void clear_from_line_1()
+{
+	move(1, 0);
+	clrtobot(); // clear until the last line
+}
+
 void draw_circle(unsigned int x, unsigned int y, unsigned int r, void (*draw)(unsigned int, unsigned int))
 {
 	const float PI = acos(-1);
 	unsigned int precision = pow(2, 8);
-	unsigned int c_x = (COLS - 1) / 2;
-	unsigned int c_y = (LINES - 1) / 2;
+	unsigned int l_x = 0;
+	unsigned int l_y = 0;
 
 	unsigned int elastic = r;
-	for (; elastic < r + 20; elastic++)
+	unsigned int max = (r + LINES - 1) / 2;
+	for (; elastic < max; elastic++)
 	{
 		// clear(); // clear screen
+		clear_from_line_1();
 		for (unsigned int step = 0; step < precision; step++)
 		{
-			move(y, x);
-			addch('o');
-			x = c_x + elastic * cos(step * (2 * PI / precision));
-			y = c_y + elastic * sin(step * (2 * PI / precision));
+			l_x = x + elastic * cos(step * (2 * PI / precision));
+			l_y = y + elastic * sin(step * (2 * PI / precision));
+			move(l_y, l_x);
+			addch('.');
 			refresh();
 		}
 		usleep(50000);
@@ -41,9 +49,12 @@ int main(void)
 {
 	initscr();
 
-	printw("Circle drawing"); // comment clear() to see this message
+	printw("Circle drawing"); // comment clear() to see this message or use clear_from_line_1()
 
-	draw_circle(70, 30, (LINES - 1) / 6, NULL);
+	unsigned int c_x = (COLS - 1) / 2;
+	unsigned int c_y = (LINES - 1) / 2;
+
+	draw_circle(c_x, c_y, (LINES - 1) / 6, NULL);
 	getch(); // hit a key to exit
 
 	endwin();
---
+++ c/coding/experiments/ncurses/ncurses_move.c
@@ -0,0 +1,84 @@
+// =============================================================================
+// Example modified from
+// https://arnaud-feltz.developpez.com/tutoriels/ncurses/?page=premier_pas
+// =============================================================================
+
+#include <ncurses.h>
+#include <math.h>
+#include <assert.h>
+#include <unistd.h>
+
+#include <time.h>
+
+typedef struct element s_element;
+
+struct element
+{
+	int x;
+	int y;
+	void (*move_element)(s_element *, int, int);
+};
+
+static void update_move_element(s_element *element, int x, int y)
+{
+	element->x += x;
+	element->y += y;
+	move(element->y, element->x);
+}
+
+void init_element(s_element *element, int x, int y)
+{
+	element->x = x;
+	element->y = y;
+	element->move_element = update_move_element;
+}
+
+static void handle_move(s_element *element)
+{
+	int key_hitten = 0;
+
+	while ((key_hitten = getch()) != 'q')
+	{
+		// clear_info();
+		switch (key_hitten)
+		{
+		case KEY_UP:
+			element->move_element(element, 0, -1);
+			break;
+		case KEY_RIGHT:
+			element->move_element(element, 1, 0);
+			break;
+		case KEY_DOWN:
+			element->move_element(element, 0, 1);
+			break;
+		case KEY_LEFT:
+			element->move_element(element, -1, 0);
+			break;
+		default:
+			printw("Arrow key expected");
+			break;
+		}
+		printw("o");
+	}
+}
+
+static void do_center()
+{
+	move((LINES - 1) / 2, (COLS - 1) / 2);
+}
+
+int main(void)
+{
+	s_element cursor;
+
+	initscr();
+	keypad(stdscr, TRUE);
+
+	do_center();
+	init_element(&cursor, getcurx(stdscr), getcury(stdscr));
+	handle_move(&cursor);
+
+	endwin();
+
+	return 0;
+}
